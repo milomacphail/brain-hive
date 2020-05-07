b4a3f0ef0ba9ef906a90ff99391744a09401ef78
milomacphail <milomacphail@gmail.com>
2020-05-07 14:33:04 -0400

    Post Demo

--- auth/server.js
+++ auth/server.js
@@ -1,15 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const users = require('./routes/user');
-const profiles = require('./routes/profile');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const db = require("./config/default").mongoURI;
+
+const posts = require("./routes/posts");
+
+mongoose
+  .connect(db)
+  .then(() => console.log("db connected successfully"))
+  .catch((error) => console.error("failed to connect to db", error));
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', users);
-app.use('/api/profiles', profiles);
+app.use("/api/posts", posts);
 
-const port = process.env.PORT || 6010;
+const port = process.env.PORT || 6020;
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, () => console.log(`server listening on port ${port}`));
